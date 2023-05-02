7af2b672cd5affa928bc6d3bce1fdc133dce0ed6
CHANDRABANDA MOHAMMED SHAKEEL <84782338+CMDSHAKEEL@users.noreply.github.com>
2023-05-02 17:45:33 +0530

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -4,15 +4,7 @@ public class HelloJava                      {
 
 	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
-		for (int i = 1;i<4;i++)             {
-			for(int j=1;j<4;j--)            {
-				
-				System.out.println("i"+i+"    "+"j"+j);
-			}
-			
-		}
-      
-      
+		System.out.println("hello world i am shakeel");
 	}
 
 }
